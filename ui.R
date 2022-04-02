9bc29c311f5e9d637c2a2a58da051540a79ed666
KennedyMwavu <mwavukennedy@gmail.com>
2022-04-02 04:34:38 +0300

    `ui.R`

---
+++ ui.R
@@ -0,0 +1,150 @@
+# ----dashboard header----
+header <- dashboardHeader(
+  title = "My Dashboard"
+)
+
+# ----dashboard sidebar-----
+sidebar <- dashboardSidebar(
+  sidebarMenu(
+    menuItem(
+      text = "Menu 1", 
+      tabName = "menu1", 
+      selected = TRUE
+    ), 
+    
+    menuItem(
+      text = "Menu 2", 
+      tabName = "menu2"
+    ), 
+    
+    menuItem(
+      text = "Menu 3", 
+      tabName = "menu3"
+    )
+  )
+)
+
+# ----dashboard body----
+body <- dashboardBody(
+  tabItems(
+    tabItem(
+      tabName = "menu1"
+    ), 
+    
+    tabItem(
+      tabName = "menu2"
+    ), 
+    
+    tabItem(
+      tabName = "menu3"
+    )
+  )
+)
+
+# ----ui----
+ui <- tabsetPanel(
+  id = "panels", 
+  type = "hidden", 
+  selected = "landing_page", 
+  
+  tabPanelBody(
+    value = "landing_page", 
+    
+    # link css stylesheet:
+    includeCSS(path = "www/css/styles.css"), 
+    
+    tags$div(
+      class = "landing_page_container", 
+      
+      tags$div(
+        class = "landing_page_header", 
+        
+        tags$div(
+          class = "landing_page_logo", 
+          
+          tags$a(
+            href = "#", 
+            
+            tags$h3(
+              tags$span(
+                tags$strong("our "), 
+                "series.org", 
+                
+                icon(
+                  name = "code", 
+                  class = "fa-solid fa-code"
+                )
+              )
+            )
+          )
+        ), 
+        
+        tags$div(
+          class = "landing_page_welcome", 
+          
+          tags$h3(
+            "Welcome!"
+          )
+        )
+      ), 
+      
+      tags$div(
+        class = "landing_page_body", 
+        
+        tags$div(
+          class = "landing_page_content", 
+          
+          tags$div(
+            class = "text_content", 
+            
+            tags$div(
+              class = "some_text", 
+              
+              tags$h1(
+                tags$strong(
+                  "TIED UP", 
+                  
+                  style = "font-size: 200%"
+                )
+              )
+            ), 
+            
+            tags$div(
+              class = "more_text", 
+              
+              tags$h3(
+                "Your Dream R Shiny Landing Page"
+              )
+            )
+          ), 
+          
+          tags$div(
+            class = "select_something", 
+            
+            selectInput(
+              inputId = "r_version", 
+              label = "", 
+              choices = c(
+                "Which Was Your First R Version?" = "", choices
+              ), 
+              selected = NULL, 
+              width = "100%"
+            )
+          )
+        )
+      )
+    )
+  ), 
+  
+  tabPanelBody(
+    value = "dashboard", 
+    
+    dashboardPage(
+      header = header, 
+      sidebar = sidebar, 
+      body = body, 
+      title = "Landing Page", 
+      skin = "green"
+    )
+  )
+)
