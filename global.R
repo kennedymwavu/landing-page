bf39216347facc6bda0ec3c457acc2aab0a6a468
KennedyMwavu <mwavukennedy@gmail.com>
2022-04-02 04:58:54 +0300

    load `{shinyjqui}` for jquery animations on UI

--- global.R
+++ global.R
@@ -1,6 +1,7 @@
 library(shiny)
 library(shinydashboard)
 library(rversions)
+library(shinyjqui)
 
 # Get all r versions, omit the ones which didn't have nicknames:
 versions <- r_versions() |> na.omit()
