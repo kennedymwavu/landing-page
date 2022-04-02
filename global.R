db07caf4ba5e2ec142b09fbf5cc4b35a9f0f0a5e
KennedyMwavu <mwavukennedy@gmail.com>
2022-04-02 05:03:10 +0300

    concise comments

--- global.R
+++ global.R
@@ -1,11 +1,12 @@
 library(shiny)
 library(shinydashboard)
-library(rversions)
-library(shinyjqui)
+library(rversions) # to get R versions
+library(shinyjqui) # for jquery animations in UI
 
 # Get all r versions, omit the ones which didn't have nicknames:
 versions <- r_versions() |> na.omit()
 
+# we'll have a `selectInput()` on the UI whose choices will be:
 choices <- c(
   "An Earlier Version", 
   paste(versions$version, versions$nickname)
